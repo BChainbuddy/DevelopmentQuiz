1757ae356b5831766cd53518086a3d6201f43709
Jaka Potokar <cc.jakapotokar@gmail.com>
2025-01-05 10:23:48 +0100

    added prisma database

--- src/app/leaderboard/page.tsx
+++ src/app/leaderboard/page.tsx
@@ -1,6 +1,12 @@
-export default function LeaderboardPage() {
-    return (
-        <>
-        </>
-    )
+import prisma from "@/lib/db";
+
+export default async function LeaderboardPage() {
+  const players = await prisma.profile.findMany({
+    orderBy: {
+      wins: "desc",
+    },
+    take: 100,
+  });
+
+  return <></>;
 }
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,11 @@
-export default function ProfilePage() {
+import prisma from "@/lib/db";
+
+export default async function ProfilePage() {
+  const profile = await prisma.profile.findUnique({
+    where: {
+      username: "jaka.potokar50@gmail.com"
+    }
+  }); // get username from next/auth
+
   return <></>;
 }
