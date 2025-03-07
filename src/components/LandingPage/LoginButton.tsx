aae01dbe8921ddb4e7d47927e7d3f6fff4f205bd
Jaka Potokar <cc.jakapotokar@gmail.com>
2025-03-07 15:30:24 +0100

    add guest login

--- src/components/LandingPage/LoginButton.tsx
+++ src/components/LandingPage/LoginButton.tsx
@@ -4,11 +4,19 @@ import { signIn } from "next-auth/react";
 
 export default function LoginButton() {
   return (
-    <button
-      className="lg:text-2xl lg:px-14 lg:py-5 md:text-xl md:px-12 md:py-4 text-lg px-6 py-3 rounded-3xl bg-[#FFFFFF90] text-black hover:bg-[#FFF] transition-all duration-300 ease-in-out"
-      onClick={() => signIn("google", { callbackUrl: "/game" })}
-    >
-      Login via GMAIL
-    </button>
+    <>
+      <button
+        className="lg:text-2xl lg:px-14 lg:py-5 md:text-xl md:px-12 md:py-4 text-lg px-6 py-3 rounded-3xl bg-[#FFFFFF90] text-black hover:bg-[#FFF] transition-all duration-300 ease-in-out"
+        onClick={() => signIn("google", { callbackUrl: "/game" })}
+      >
+        Login via GMAIL
+      </button>
+      <button
+        onClick={() => signIn("credentials", { callbackUrl: "/game" })}
+        className="lg:text-lg md:text-base text-sm text-white hover:text-white/60 transition-colors duration-300 ease-in-out border border-white rounded-2xl px-2 py-0.5"
+      >
+        Continue as Guest
+      </button>
+    </>
   );
 }
--- src/lib/nextAuth.ts
+++ src/lib/nextAuth.ts
@@ -1,12 +1,21 @@
 import { createProfile } from "@/actions/actions";
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions: NextAuthOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID ?? "",
       clientSecret: process.env.GOOGLE_SECRET ?? "",
+    }),
+    CredentialsProvider({
+      name: "Guest",
+      credentials: {},
+      async authorize(credentials, req) {
+        // For testing purposes, simply return a guest user object.
+        return { id: "guest", name: "Guest User", email: "guest@example.com" };
+      },
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
